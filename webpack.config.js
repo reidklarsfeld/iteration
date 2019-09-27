9f277dcc935de342819343472242b37daf4f5f9d
Reid Klarsfeld <reidklarsfeld@Reids-MacBook-Pro.local>
2019-09-26 17:28:06 -0700

    pulled from master on thursday afternoon

--- webpack.config.js
+++ webpack.config.js
@@ -23,19 +23,6 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
         exclude: /node_modules/
       },
-      // {
-      //   test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg|ico)$/,
-      //   use: [
-      //     {
-      //       // loads files as base64 encoded data url if image file is less than set limit
-      //       loader: 'url-loader',
-      //       options: {
-      //         // if file is greater than the limit (bytes), file-loader is used as fallback
-      //         limit: 8192,
-      //       },
-      //     },
-      //   ],
-      // }
     ]
   },
   watch: true,
